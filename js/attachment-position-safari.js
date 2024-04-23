2a9779951a04f3c14de7faaeba11048c97efe7c8
marianamartsy <mart7mariana@gmail.com>
2024-04-23 23:45:21 +0200

    js for mobile safari added

---
+++ js/attachment-position-safari.js
@@ -0,0 +1,17 @@
+const header = document.querySelector('.header')
+const heroImg = document.querySelector('.hero-img')
+function isMobile() {
+	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+}
+
+function isSafariMobile() {
+	return /Safari/i.test(navigator.userAgent) && /iPhone|iPad|iPod/i.test(navigator.userAgent)
+}
+
+if (isMobile() && isSafariMobile()) {
+	header.style.backgroundAttachment = 'scroll'
+    heroImg.style.backgroundAttachment = 'scroll'
+} else {
+	header.style.backgroundAttachment = 'fixed'
+    heroImg.style.backgroundAttachment = 'fixed'
+}
