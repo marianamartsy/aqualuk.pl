0cdfcce85c0071456a544abba724db9a413df8ae
marianamartsy <mart7mariana@gmail.com>
2024-06-17 20:49:27 +0200

    changed

--- js/producers.js
+++ js/producers.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$('.producers__boxes').slick({
 		slidesToShow: 3,
-		slidesToScroll: 1,
+		slidesToScroll: 3,
 		autoplay: true,
 		autoplaySpeed: 2500,
 		arrows: false,
